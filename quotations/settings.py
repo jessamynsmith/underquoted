247ad74dea78c3670d772c00734147075f9f6b27
Jessamyn Smith <geekchick77@gmail.com>
2013-01-27 20:11:41 -0500

    Created empty quotes app

--- quotations/settings.py
+++ quotations/settings.py
@@ -121,6 +121,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
     'south',
     'tastypie',
+    'quotations.apps.quotes',
 )
 
 # A sample logging configuration. The only tangible logging
