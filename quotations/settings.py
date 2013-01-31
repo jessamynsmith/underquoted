631e9870afb36dd21867400d18d79a1a6a6ead80
Jessamyn Smith <geekchick77@gmail.com>
2013-01-31 11:25:39 -0500

    Broke up settings into dev and prod

--- quotations/settings.py
+++ quotations/settings.py
@@ -1,24 +1,11 @@
 # Django settings for quotations project.
-
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+import os
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    (os.environ['ADMIN_NAME'], os.environ['ADMIN_EMAIL']),
 )
 
 MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'quotations',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
---
+++ quotations/settings_prod.py
@@ -0,0 +1,8 @@
+from settings import *
+
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES = {
+    'default': dj_database_url.config()
+}
