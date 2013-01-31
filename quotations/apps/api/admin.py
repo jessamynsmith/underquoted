83e978b98cb6a4a133fbc6eff9190436cf71d18f
Jessamyn Smith <geekchick77@gmail.com>
2013-01-31 18:01:02 -0500

    Added s3 storage as well, and made api keys be created when a user is created

--- quotations/apps/api/admin.py
+++ quotations/apps/api/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from tastypie.models import ApiKey
+from django.contrib.auth.models import User
+from django.db import models
+from tastypie.models import ApiKey, create_api_key
+
+models.signals.post_save.connect(create_api_key, sender=User)
 
 admin.site.register(ApiKey)
--- quotations/settings.py
+++ quotations/settings.py
@@ -121,6 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    's3_folder_storage',
     'south',
     'storages',
     'tastypie',
