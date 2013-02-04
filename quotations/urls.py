3ec0e462bd3e52e25d9c91ae8d10ed5ba3bf68f4
Jessamyn Smith <geekchick77@gmail.com>
2013-02-04 14:55:58 -0500

    Oh what a difference a $ makes (especially in a regex)

--- quotations/urls.py
+++ quotations/urls.py
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    url(r'^', quotation_views.random),
+    url(r'^$', quotation_views.random),
     url(r'^random/', quotation_views.random),
 
     url(r'^admin/', include(admin.site.urls)),
