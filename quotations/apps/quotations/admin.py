527a427a2d2184cfa8a8432be369b3c50744551f
Jessamyn Smith <geekchick77@gmail.com>
2013-11-19 21:46:24 -0500

    Added search function to admin for quotation and author

--- quotations/apps/quotations/admin.py
+++ quotations/apps/quotations/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from quotations.apps.quotations import models
 
-admin.site.register(models.Author)
-admin.site.register(models.Quotation)
+
+class AuthorAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+class QuotationAdmin(admin.ModelAdmin):
+    search_fields = ['text', 'author__name']
+
+
+admin.site.register(models.Author, AuthorAdmin)
+admin.site.register(models.Quotation, QuotationAdmin)
