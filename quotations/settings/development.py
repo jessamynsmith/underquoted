ae5e7796694bcfef247119df1e89225b2e34b3ef
Jessamyn Smith <geekchick77@gmail.com>
2015-01-29 14:43:22 -0500

    Removed TEST_NAME

--- quotations/settings/development.py
+++ quotations/settings/development.py
@@ -14,7 +14,6 @@ DATABASES = {
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-        'TEST_NAME': 'test_quotations',
     }
 }
 
