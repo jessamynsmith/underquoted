1f0c54c1f90d79bec91bc9b145103ce9121b4e37
Jessamyn Smith <geekchick77@gmail.com>
2015-01-29 16:57:47 -0500

    Upgraded all the packages!

--- quotations/apps/quotations/migrations/0001_initial.py
+++ quotations/apps/quotations/migrations/0001_initial.py
@@ -1,20 +1,36 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        pass
+    dependencies = [
+    ]
 
-    def backwards(self, orm):
-        pass
-
-    models = {
-
-    }
-
-    complete_apps = ['quotations']
+    operations = [
+        migrations.CreateModel(
+            name='Author',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=100)),
+                ('date_of_birth', models.DateField()),
+                ('date_of_death', models.DateField(null=True, blank=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Quotation',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('text', models.CharField(unique=True, max_length=500)),
+                ('author', models.ForeignKey(related_name='quotations', to='quotations.Author')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- quotations/settings/production.py
+++ quotations/settings/production.py
@@ -131,7 +131,6 @@ INSTALLED_APPS = [
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     's3_folder_storage',
-    'south',
     'storages',
     'tastypie',
     'quotations.apps.api',
@@ -139,32 +138,3 @@ INSTALLED_APPS = [
 ]
 
 MAX_PER_PAGE = 5
-
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
