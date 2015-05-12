1f98c640358f620cc93c80ca0d8e96d0501cdc1b
Jessamyn Smith <geekchick77@gmail.com>
2015-05-12 18:05:18 -0400

    Order authors alphabetically

--- quotations/models.py
+++ quotations/models.py
@@ -5,6 +5,10 @@ from djorm_pgfulltext.models import SearchManager
 
 
 class Author(models.Model):
+
+    class Meta:
+        ordering = ('name',)
+
     name = models.CharField(max_length=100, unique=True)
     date_of_birth = models.DateField()
     date_of_death = models.DateField(null=True, blank=True)
