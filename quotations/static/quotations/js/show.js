85e056b8414d16627615a497231d73d762834230
jessamynsmith <jessamyn.smith@gmail.com>
2017-11-09 16:39:03 -0700

    Add event_action to customized event

--- quotations/static/quotations/js/show.js
+++ quotations/static/quotations/js/show.js
@@ -1,18 +1,10 @@
 $(document).ready(function () {
   gtag('event', 'page_view');
 
-  var result = gtag('event', 'purchase_attempt', {
+  gtag('event', 'purchase_attempt', {
     'event_label': 'single',
-    'purchase_type': 'single',
-    'article_price': '5'
+    'event_action': '5'
   });
-  console.log(result);
-
-  var result = gtag('event', 'purchase', {
-    'purchase_type': 'single',
-    'article_price': '5'
-  });
-  console.log(result);
 
   $("#search").on('click', function (event) {
     gtag('event', 'search', {
