4afe31efc7d059517d465df4c304f3a769a0e934
jessamynsmith <jessamyn.smith@gmail.com>
2017-11-09 16:48:58 -0700

    Have just a single custom event on search.

--- quotations/static/quotations/js/show.js
+++ quotations/static/quotations/js/show.js
@@ -1,14 +1,10 @@
 $(document).ready(function () {
-  gtag('event', 'page_view');
-
-  gtag('event', 'purchase_attempt', {
-    'event_action': 'single',
-    'event_label': '5'
-  });
 
   $("#search").on('click', function (event) {
-    gtag('event', 'search', {
-      'search_term': $("#search_text").val()
+    gtag('event', 'purchase_attempt', {
+      'event_action': 'search',
+      'event_label': $("#search_text").val()
     });
   });
+
 });
